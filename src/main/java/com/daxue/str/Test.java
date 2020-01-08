76cff5fcc9fce603b4fec8ec181286cfbd7a86dc
wangxuedi <wangxuedi@izton.com>
2020-01-08 15:40:24 +0800

    提交

--- src/main/java/com/daxue/str/Test.java
+++ src/main/java/com/daxue/str/Test.java
@@ -3,6 +3,29 @@ package com.daxue.str;
 import java.util.Date;
 
 public class Test {
+
+    public static Boolean ValueOf(boolean b) {
+        return b ? Boolean.TRUE : Boolean.FALSE;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public static void main(String[] args) {
 
         long time = new Date().getTime();
